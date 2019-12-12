56b9ef73f312a782a1aeef6c079bed5f7b2123ac
Mateusz <mateusz.czubkowski@gmail.com>
2019-12-12 13:07:14 +0100

    changes in forms

---
+++ CourierApp/CourierApp.Core/ViewModels/Packages/CreatePackageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CourierApp.Core.ViewModels.Packages
+{
+    public class CreatePackageViewModel
+    {
+        [Required(ErrorMessage = "Adres doręczenia jest wymagany")]
+        [Display(Name = "Adres doręczenia")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "E-mail klienta jest wymagany")]
+        [EmailAddress(ErrorMessage = "Niepoprawny adres e-mail klienta")]
+        [Display(Name = "E-mail klienta")]
+        public string Email { get; set; }
+
+        [Display(Name = "Kurier")]
+        public int CourierId { get; set; }
+
+        public IEnumerable<SelectListItem> Couriers { get; set; }
+    }
+}
