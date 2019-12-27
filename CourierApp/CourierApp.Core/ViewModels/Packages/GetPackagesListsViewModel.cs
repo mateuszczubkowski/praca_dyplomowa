bd8a86b202f353a11b20bca21e7204cc9d043380
Mateusz <mateusz.czubkowski@gmail.com>
2019-12-27 12:26:36 +0100

    second list with delivered packages for courier, working redirect

---
+++ CourierApp/CourierApp.Core/ViewModels/Packages/GetPackagesListsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierApp.Core.ViewModels.Packages
+{
+    public class GetPackagesListsViewModel
+    {
+        public IEnumerable<PackagesListViewModel> Delivered { get; set; }
+
+        public IEnumerable<PackagesListViewModel> InProgress { get; set; }
+    }
+}
